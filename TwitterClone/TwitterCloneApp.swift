055565b726e14e6acf25f3f4e3337bd45d4a0e9e
Giorgi samkhar <padregiorgi777@gmail.com>
2022-12-19 20:22:19 +0400

    Twitter Clone starter project

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Giorgi Samkharadze on 19.12.22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
